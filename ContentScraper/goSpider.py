bd81ce917b0eb200c9629ff35ff0787c5c5026a8
10CodeDev <10CodeDevelopment@gmail.com>
2021-02-10 19:11:41 +0300

    V1.0

--- ContentScraper/goSpider.py
+++
@@ -1,7 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from data.spiders.dataSpider import DataSpider
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(DataSpider)
-process.start()
