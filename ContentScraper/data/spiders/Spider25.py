a5ca334188b7476551644311fd3feddf6af44c97
10CodeDev <10CodeDevelopment@gmail.com>
2021-04-03 20:44:24 +0300

    New Spider + Updated Source.html page

---
+++ ContentScraper/data/spiders/Spider25.py
@@ -0,0 +1,25 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from data.items import DataItem
+
+
+class SpiderEighteen(Spider):
+    name = "data"
+
+    allowed_domains = [
+        "www.abovethethrone.com",
+    ]
+
+    start_urls = [
+        "https://www.islamhomosexuality.com/hs/authors/Admin.cfm",
+    ]
+
+    def parse(self, response):
+        scrapedData = Selector(response).css('a.articleTitleList')
+
+        for data in scrapedData:
+            item = DataItem()
+            item['title'] = data.css('a.articleTitleList::text').get(),
+            item['source'] = 'IslamHomosexuality.com - Article - Abu Iyyad',
+            item['url'] = data.css('a.articleTitleList::attr(href)').get()
+            yield item
