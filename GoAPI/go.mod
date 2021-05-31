42936ecaa68497231f27a4ca8be7dd2c4c09def8
10CodeDev <10CodeDevelopment@gmail.com>
2021-05-31 23:21:39 +0300

    Go-API Build Working

--- GoAPI/go.mod
+++ GoAPI/go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/go-redis/redis/v8 v8.8.3
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	go.mongodb.org/mongo-driver v1.5.2
 )
--- GoAPI/main.go
+++ GoAPI/main.go
@@ -8,14 +8,13 @@ import (
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	RedisAddr string = "localhost:6379"
-	RedisPass string = "password"
+	RedisAddr string = "redis-api:6379"
+	RedisPass string = "Password:)"
 	RCtx             = context.Background()
 	MCtx             = context.TODO()
 )
@@ -60,57 +59,78 @@ type MongoDoc struct {
 
 // Function: Get Results From Redis
 func redisResults(identifier string) string {
-	db := RedisDB(0)
+	db := RedisDB(1)
 	defer db.Close()
 	data := db.Get(RCtx, identifier)
 	return data.String()
 }
 
 // Function: Get Results From MongoDB
-func mongoResults(identifier string) MongoDoc {
-
-	var results MongoDoc
-
-	db := MongoDB("localhost", "27018")
-	defer db.Disconnect(MCtx)
-	coll := db.Database("SearchEngineDB").Collection("htmlResults")
-	filter := bson.D{{Key: "_id", Value: identifier}}
-	err := coll.FindOne(MCtx, filter).Decode(&results)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return results
-}
+//func mongoResults(identifier string) MongoDoc {
+//
+//	var results MongoDoc
+//
+//	db := MongoDB("mongo-se", "27018")
+//	defer db.Disconnect(MCtx)
+//	coll := db.Database("SearchEngineDB").Collection("htmlResults")
+//	filter := bson.D{{Key: "_id", Value: identifier}}
+//	err := coll.FindOne(MCtx, filter).Decode(&results)
+//
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	return results
+//}
 
 // Receive Query from Client
 func queryAPI(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
 	identifier := r.FormValue("identifier")
 	query := r.FormValue("query")
 
-	if identifier == nil {
-		log.Fatal("NEEDS TO BE COMPLETED")
-	}
+	db := RedisDB(0)
+	defer db.Close()
 
-	if query == nil {
-		log.Fatal("NEEDS TO BE COMPLETED")
-	}
+	db.XAdd(RCtx, &redis.XAddArgs{
+		Stream:       "streamA",
+		MaxLen:       0,
+		MaxLenApprox: 0,
+		ID:           "",
+		Values: map[string]interface{}{
+			"identifier": string(identifier),
+			"query":      string(query),
+		},
+	})
+
+	json.NewEncoder(w).Encode(struct {
+		StatusCode int
+		Message    string
+	}{200, "OK"})
 }
 
 // Provide Results to Client
 func resultsAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
+
 	params := mux.Vars(r)
 	identifier := params["identifier"]
 
 	results := redisResults(identifier)
-	json.NewEncoder(w).Encode(results)
+
+	json.NewEncoder(w).Encode(struct {
+		Data string
+	}{results})
 }
 
 // Provide Metrix & Stats
 func metrix(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 
+	json.NewEncoder(w).Encode(struct {
+		Message string
+	}{"Not Implemented Yet"})
 }
 
 // Create API Endpoints
