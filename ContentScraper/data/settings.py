c499762cf4051ab8906b13859ff75187807eccff
AbdullahRehmat <rehmat.dev@gmail.com>
2021-12-02 16:49:37 +0300

    Refactored Code

--- ContentScraper/data/settings.py
+++ ContentScraper/data/settings.py
@@ -26,7 +26,7 @@ MONGO_COLLECTION = "Test"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
