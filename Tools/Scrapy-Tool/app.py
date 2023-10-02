66b7f0d864961ecaaafa36310d8b6fe20806f54b
AbdullahRehmat <rehmat.dev@gmail.com>
2023-10-02 19:57:14 +0300

    Updated Tools

--- Tools/Scrapy-Tool/app.py
+++ Tools/Scrapy-Tool/app.py
@@ -4,7 +4,17 @@ from scrapy.crawler import CrawlerProcess
 from spellchecker import SpellChecker
 
 
-class TestSpider(scrapy.Spider):
+def splitAuthor(scrapedTitle) -> list:
+    """ Returns List Containing Title & Authors Name If Present """
+
+    if " By " in scrapedTitle:
+        return scrapedTitle.rsplit(" By ", 1)
+
+    else:
+        return [scrapedTitle, ""]
+
+
+class SpiderB(scrapy.Spider):
     name = 'testspider'
 
     start_urls = ["https://www.salafisounds.com/category/speakers/"]
@@ -13,37 +23,25 @@ class TestSpider(scrapy.Spider):
         scrapedData = Selector(response).css('h3.mh-posts-grid-title')
 
         for data in scrapedData:
-            #title = data.css('h3.mh-posts-grid-title > a::attr(title)').get()
-            #url = data.css('h3.mh-posts-grid-title > a::attr(href)').get()
 
             scrapedTitle = data.css(
                 'h3.mh-posts-grid-title > a::attr(title)').get()
 
             sc = SpellChecker()
             scrapedTitle = sc.run_spell_checker(scrapedTitle)
+            print(scrapedTitle)
 
-            if "by " in scrapedTitle:
-                title = scrapedTitle.rsplit(" by ", 1)
-                print(title[0])
-                print(title[1])
-                print(title)
-                print()
-
-            elif "By " in scrapedTitle:
-                title = scrapedTitle.rsplit(" By ", 1)
-                print(title[0])
-                print(title[1]) 
-                print(title)
-                print()
-            else:
-                print(title[0])
-                print(title[1]) 
-                print()
+            scrapedTitle = splitAuthor(scrapedTitle)
+            print(scrapedTitle[0])
+            print(scrapedTitle[1])
+            print()
 
 
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
+if __name__ == "__main__":
 
-process.crawl(TestSpider)
-process.start()
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+
+    process.crawl(SpiderB)
+    process.start()
