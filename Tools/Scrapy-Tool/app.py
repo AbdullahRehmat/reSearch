df5471130411a9e20f3a9009a033a12e207a5739
AbdullahRehmat <rehmat.dev@gmail.com>
2023-10-02 19:01:39 +0300

    Updated Tools

--- Tools/Scrapy-Tool/app.py
+++ Tools/Scrapy-Tool/app.py
@@ -1,5 +1,7 @@
 import scrapy
 from scrapy import Selector
+from scrapy.crawler import CrawlerProcess
+from spellchecker import SpellChecker
 
 
 class TestSpider(scrapy.Spider):
@@ -11,12 +13,37 @@ class TestSpider(scrapy.Spider):
         scrapedData = Selector(response).css('h3.mh-posts-grid-title')
 
         for data in scrapedData:
-            title = data.css('h3.mh-posts-grid-title > a::attr(title)').get()
-            url = data.css('h3.mh-posts-grid-title > a::attr(href)').get()
+            #title = data.css('h3.mh-posts-grid-title > a::attr(title)').get()
+            #url = data.css('h3.mh-posts-grid-title > a::attr(href)').get()
 
-        print(" ")
-        print("#"*25)
-        print(title)
-        print(url)
-        print("#"*25)
-        print(" ")
+            scrapedTitle = data.css(
+                'h3.mh-posts-grid-title > a::attr(title)').get()
+
+            sc = SpellChecker()
+            scrapedTitle = sc.run_spell_checker(scrapedTitle)
+
+            if "by " in scrapedTitle:
+                title = scrapedTitle.rsplit(" by ", 1)
+                print(title[0])
+                print(title[1])
+                print(title)
+                print()
+
+            elif "By " in scrapedTitle:
+                title = scrapedTitle.rsplit(" By ", 1)
+                print(title[0])
+                print(title[1]) 
+                print(title)
+                print()
+            else:
+                print(title[0])
+                print(title[1]) 
+                print()
+
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+process.crawl(TestSpider)
+process.start()
---
+++ Tools/Scrapy-Tool/spellchecker.py
@@ -0,0 +1,101 @@
+"""
+    Basic Spell Checker & Punctuation Stripper.
+    Used To Standarise Spelling According To 
+    Provided JSON Dictionary.
+"""
+
+import os
+import json
+import unicodedata
+
+
+class SpellChecker:
+    def __init__(self) -> None:
+        self.wl_file = "wordlist.json"
+        self.wordlist = {}
+
+    def _load_wordlist(self) -> None:
+        """ Loads JSON Wordlist From Local Directory """
+
+        path = os.path.join(os.path.dirname(__file__), self.wl_file)
+        file = open(path)
+        data = json.load(file)
+        self.wordlist = data["dictionary"]
+
+    def strip_punctuation(self, text: str) -> str:
+        """ Removes Excess Punctuation """
+
+        punctuation = "!#$%'*+,./;<=>?@[\]^_`{|}~()"
+        new_text = text.translate(str.maketrans("", "", punctuation))
+
+        return new_text
+
+    def strip_accents(self, text: str) -> str:
+        """ Removes Accent Marks From Letters """
+
+        nfkd_form = unicodedata.normalize("NFKD", text)
+        ascii_form = nfkd_form.encode("ASCII", "ignore")
+        new_text = str(ascii_form.decode("utf-8"))
+
+        return new_text
+
+    def format_spaces(self, text: str) -> str:
+        """ Removes Double, Leading & Trailing Spaces """
+
+        # Remove Double Spaces
+        if "  " in text:
+            text = text.replace("  ", " ")
+
+        else:
+            pass
+
+        # Remove Leading & Trailing Spaces
+        new_text = text.strip()
+
+        return new_text
+
+    def fix_spelling(self, text: str) -> str:
+        """ Correct Spelling In Accordance To Provided Wordlist """
+
+        # Load JSON Wordlist File Into Memory
+        self._load_wordlist()
+
+        # k = Correctly spelled word
+        # v = [Incorrectly spelled versions of k]
+        # i = Current position in v
+
+        for k, v in self.wordlist.items():
+            for i in v:
+                if i.lower() in text:
+                    new_text = text.replace(i.lower(), k.title())
+                    return new_text
+
+                elif i.title() in text:
+                    new_text = text.replace(i.title(), k.title())
+                    return new_text
+
+        else:
+            return text
+
+    def run_spell_checker(self, text: str) -> str:
+        """ Runs All Functions On Input """
+
+        if type(text) != str:
+            return str(text)
+
+        text = self.strip_punctuation(text)
+        text = self.strip_accents(text)
+        text = self.format_spaces(text)
+        text = self.fix_spelling(text)
+
+        return text
+
+
+if __name__ == "__main__":
+
+    print("Library Running In TEST MODE...")
+    input_text = input("Enter Text: ")
+
+    s = SpellChecker()
+    corrected_text = s.run_spell_checker(input_text)
+    print("Corrected Text: " + corrected_text)
